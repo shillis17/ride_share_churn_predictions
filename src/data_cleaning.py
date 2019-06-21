230639259a338b3494b0344ae5d9a9bae0f272a7
Nicolas Jacobsohn <nicolasjacobsohn@Nicolass-MacBook-Pro.local>
2019-06-21 10:54:13 -0600

    added cleaning script

---
+++ src/data_cleaning.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime
+
+
+df_train = pd.read_csv("../data/churn_train.csv")
+df_holdout = pd.read_csv("../data/churn_test.csv")
+df_ls = [df_train, df_holdout]
+for df in df_ls:
+    df['last_trip_date'] = pd.to_datetime(df['last_trip_date'])
+    df['signup_date'] = pd.to_datetime(df['signup_date'])
+    df["churn"] = df['last_trip_date'].copy()
+    churn_date = datetime.datetime(2014, 6, 1)
+    churn_true = set(df[df["churn"] >= churn_date]["churn"].index)
+    churn_false = set(df[df["churn"] != True]["churn"].index)
+    df.loc[churn_true, "churn"] = True
+    df.loc[churn_false, "churn"] = False
+
+df_train.to_csv("data/clean_train.csv")
+df_holdout.to_csv("data/clean_holdout.csv")
